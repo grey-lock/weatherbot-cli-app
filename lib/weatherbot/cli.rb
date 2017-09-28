4342ca1bc043440818e4872a6e97c48a5fd81da8
TheInvalidNonce <TheInvalidNonce@users.noreply.github.com>
2017-09-28 12:59:17 -0400

    Stub input options, not all options available yet.

--- lib/weatherbot/cli.rb
+++ lib/weatherbot/cli.rb
@@ -63,24 +63,45 @@ class Weatherbot::CLI
 
   def list_popular_cities
     puts ""
-    puts "London, United Kingdom"
-    puts "New York City, United States"
-    puts "Shanghai, China"
-    puts "Tokyo, Japan"
-    puts "Berlin, Germany"
-    puts "Lagos, Nigeria"
-    puts "Istanbul, Turkey"
-    puts "Mumbai, India"
-    puts "Moscow, Russia"
-    puts "São Paulo, Brazil"
+    puts "1. London, United Kingdom"
+    puts "2. New York City, United States"
+    puts "3. Shanghai, China"
+    puts "4. Tokyo, Japan"
+    puts "5. Berlin, Germany"
+    puts "6. Lagos, Nigeria"
+    puts "7. Istanbul, Turkey"
+    puts "8. Mumbai, India"
+    puts "9. Moscow, Russia"
+    puts "10. São Paulo, Brazil"
     puts ""
   end
 
   def menu
     input = nil
+
     puts ""
-    puts "You can select from popular cities below, or enter a location in the format: <city>, <country>, to check the current weather conditions for that location. Or type 'exit' to quit."
+    puts "You can select from popular cities above by typing 1-10, or enter a location in the format: <city>, <country> to check the current weather conditions for that location. Or type 'exit' to quit."
     puts ""
+
+    while input != "exit"
+
+      input = gets.strip.downcase
+
+      case input
+        when "1"
+          puts "London, United Kingdom current weather"
+        when "2"
+          puts "New York City, United States current weather"
+        when "3"
+          puts "Shanghai, China current weather"
+
+        when "exit"
+          puts "See you again soon!"
+        else
+          puts "Invalid selection, please try again."
+        end
+      end
+      
   end
 
 
