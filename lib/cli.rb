7fe37516bca1ed17e85afa53a087f64bb7961fe5
TheInvalidNonce <TheInvalidNonce@users.noreply.github.com>
2017-10-19 17:41:38 -0400

    Add list of to menu of previous inputs

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,7 @@
 
 class Weatherbot::CLI
 
+
   def call
     puts "
            __   __  ___           ___   __   __   __
@@ -38,6 +39,7 @@ class Weatherbot::CLI
       puts "\n-------------------------------\n"
       puts "\nPlease enter a specific location in the format: <location>, <country> to check the current weather conditions for that location. You can also search by <location>, <state/region>, <country> to find the correct specific location.
       \nType 'map' to open the most relevant result in your OS default browser.
+      \nType 'list' to see a list of previous locations.
       \n*NOTE: This will open your web browser to the most likely location coordinates in Google Maps.*
       \nYou can also type 'forecast' to display the 3 day forecast of the most recent search. To quit, type 'exit'.\n"
       puts "\n-------------------------------\n"
@@ -56,7 +58,12 @@ class Weatherbot::CLI
       end
 
       # Display current weather
-      Weatherbot::API.current_weather(input)
+      weather = Weatherbot::API.current_weather(input)
+
+
+
+      display_weather(weather)
+
       new_input = gets.chomp.downcase
 
       # Check for specific input commands
@@ -68,6 +75,8 @@ class Weatherbot::CLI
       if new_input === "forecast"
         Weatherbot::API.forecast(input)
         menu
+      elsif new_input === "list"
+        display_previous
       elsif new_input === "map"
         Weatherbot::API.open_link
         menu
@@ -78,6 +87,25 @@ class Weatherbot::CLI
 
       end
 
+  end
+
+  def display_weather(weather)
+    puts "\n\nReport Time:      #{weather.report_time}"
+    puts "Location:         #{weather.location_name}, #{weather.country}"
+    puts "Coordinates:      #{weather.coordinates}"
+    puts "Google Maps:      #{weather.google_maps}"
+    puts "\nTemperature:      #{weather.temp_avg}ºF / #{weather.temp_celsius}ºC"
+    puts "Condition:        #{weather.condition.capitalize}"
+    puts "Cloudiness:       #{weather.cloudiness}%"
+    puts "\nHumidity:         #{weather.humidity}%"
+    puts "Wind Speed:       #{weather.wind_speed} mph"
+    puts "Wind Direction:   #{weather.wind_direction}"
+    puts "\nSunrise:          #{weather.sunrise}"
+    puts "Sunset:           #{weather.sunset}"
+  end
+
+  def display_previous
+    Weatherbot::API.locations.map { |entry| puts entry.location_name }
   end
 
 
